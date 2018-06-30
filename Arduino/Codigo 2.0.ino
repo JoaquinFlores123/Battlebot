c618d10d547cf6b41da53b8ba5c59e209c2e30d3
stkierke <38736930+stkierke@users.noreply.github.com>
2018-06-30 15:49:53 -0400

    Add files via upload

---
+++ Arduino/Codigo 2.0.ino
@@ -0,0 +1,54 @@
+char nombre[10] = "Buldrini";
+char password[10] = "1234";
+char baud = '4';
+char entrada;
+
+void motores(int l);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()>=1){
+    entrada = Serial.read();
+    motores(entrada);
+   }
+}
+
+void motores(int l){
+  if (l=='F'){
+    digitalWrite(7, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+  }
+  else if (l=='B'){
+    digitalWrite(7, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(4, HIGH);
+  }
+  else if (l=='R'){
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+  }
+  else if (l=='L'){
+    digitalWrite(7, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+  }
+  else {
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+  }
+}
